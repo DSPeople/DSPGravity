77765817e78b9f82070dd401d242b3953b614403
Saix_93 <r.torresgonzalez.93@gmail.com>
2017-08-12 14:47:36 +0200

    Añade movimiento al personaje y la camara. Añade control de tecla para cambiar la gravedad del personaje. Importa sprites de ladrillos y de personaje Crea animación para el personaje Crea capas físicas para personaje y ladrillos Crea material físico para el personaje

---
+++ Assets/GameAssets/_Scripts/Camera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    [Header("Camera properties")]
+    [SerializeField]
+    private Character _character;
+
+    private Rigidbody2D _rigidbody;
+
+    private void Awake()
+    {
+        _rigidbody = this.GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody.AddForce(Vector2.right * Time.deltaTime * _character.GetSpeed(), ForceMode2D.Force);
+    }
+}
---
+++ Assets/GameAssets/_Scripts/Character.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    [Header("Character properties")]
+    [SerializeField]
+    private float _speed = 1;
+    [SerializeField]
+    private LayerMask _layerMask;
+
+    private Rigidbody2D _rigidbody;
+
+    private void Awake()
+    {
+        _rigidbody = this.GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && _rigidbody.IsTouchingLayers(_layerMask))
+        {
+            _rigidbody.gravityScale *= -1;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody.AddForce(Vector2.right * Time.deltaTime * _speed, ForceMode2D.Force);
+    }
+
+    public float GetSpeed()
+    {
+        return _speed;
+    }
+    
+    public void SetSpeed(float newSpeed)
+    {
+        _speed = newSpeed;
+    }
+}
